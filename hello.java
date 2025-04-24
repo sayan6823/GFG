4d06e3091c1f7b5da47f9b519219f03d46ab9cac
sayan6823 <sayanchat6823@gmail.com>
2025-04-24 15:45:15 +0530

    Added

---
+++ hello.java
@@ -0,0 +1,20 @@
+public class hello {
+    public static void main(String[] args) {
+
+        System.out.println(7 & 3);   // ?
+        System.out.println(7 | 3);   // ?
+        System.out.println(7 ^ 3);   // ?
+        System.out.println(~7);      // ?
+        System.out.println(7 << 1);  // ?
+        System.out.println(7 >> 1);  // ?
+        
+        // int myNum= 2;
+        // float myFloat= 3.8f;
+        // double myDouble= 3.8;
+        // char myChar= 'S';
+        // boolean myBoolean= true;
+        // String myString= "Sayan";
+
+        // System.out.println(myNum + 3);
+    }
+}
